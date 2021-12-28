2d3f400344138b953ee1066860b114e9b5ae5c5a
Alequin <j-cox03@hotmail.com>
2021-12-28 19:13:29 +0000

    timer now counts down if the start date is in the future

--- src/get-time-since.js
+++ src/get-time-since.js
@@ -1,9 +1,10 @@
 export const getTimeSince = (startDate, endDate = new Date()) => {
-  const millisecondsPast = endDate.getTime() - startDate.getTime();
+  const millisecondsPast = Math.abs(endDate.getTime() - startDate.getTime());
 
   const days = millisecondsToDays(millisecondsPast);
   const daysToDisplay = Math.floor(days);
-  const remainingDaysInMilliseconds = millisecondsPast - daysToMilliseconds(daysToDisplay);
+  const remainingDaysInMilliseconds =
+    millisecondsPast - daysToMilliseconds(daysToDisplay);
 
   const hours = millisecondsToHours(remainingDaysInMilliseconds);
   const hoursToDisplay = Math.floor(hours);
@@ -27,12 +28,18 @@ export const getTimeSince = (startDate, endDate = new Date()) => {
 };
 
 const millisecondsToMilliseconds = (numOfMilliseconds) => numOfMilliseconds;
-const secondsToMilliseconds = (numOfSeconds) => millisecondsToMilliseconds(numOfSeconds) * 1000;
-const minutesToMilliseconds = (numOfMinutes) => secondsToMilliseconds(numOfMinutes) * 60;
-const hoursToMilliseconds = (numOfHours) => minutesToMilliseconds(numOfHours) * 60;
+const secondsToMilliseconds = (numOfSeconds) =>
+  millisecondsToMilliseconds(numOfSeconds) * 1000;
+const minutesToMilliseconds = (numOfMinutes) =>
+  secondsToMilliseconds(numOfMinutes) * 60;
+const hoursToMilliseconds = (numOfHours) =>
+  minutesToMilliseconds(numOfHours) * 60;
 const daysToMilliseconds = (numOfDays) => hoursToMilliseconds(numOfDays) * 24;
 
 const millisecondsToSeconds = (milliseconds) => milliseconds / 1000;
-const millisecondsToMinutes = (milliseconds) => millisecondsToSeconds(milliseconds) / 60;
-const millisecondsToHours = (milliseconds) => millisecondsToMinutes(milliseconds) / 60;
-const millisecondsToDays = (milliseconds) => millisecondsToHours(milliseconds) / 24;
+const millisecondsToMinutes = (milliseconds) =>
+  millisecondsToSeconds(milliseconds) / 60;
+const millisecondsToHours = (milliseconds) =>
+  millisecondsToMinutes(milliseconds) / 60;
+const millisecondsToDays = (milliseconds) =>
+  millisecondsToHours(milliseconds) / 24;
--- src/get-time-since.test.js
+++ src/get-time-since.test.js
@@ -2,7 +2,12 @@ import { getTimeSince } from "./get-time-since";
 
 describe("get-time-since", () => {
   it("returns zero for all time units when the start time and end time match", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T00:00:10"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-01T00:00:10")
+      )
+    ).toEqual({
       days: 0,
       hours: 0,
       minutes: 0,
@@ -11,7 +16,12 @@ describe("get-time-since", () => {
   });
 
   it("returns 1 second when the start time is 00:00:00 and the end time is 00:00:01", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T00:00:01"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-01T00:00:01")
+      )
+    ).toEqual({
       days: 0,
       hours: 0,
       minutes: 0,
@@ -20,7 +30,12 @@ describe("get-time-since", () => {
   });
 
   it("returns 59 seconds when the start time is 00:00:00 and the end time is 00:00:59", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T00:00:59"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-01T00:00:59")
+      )
+    ).toEqual({
       days: 0,
       hours: 0,
       minutes: 0,
@@ -29,7 +44,12 @@ describe("get-time-since", () => {
   });
 
   it("returns 1 minute & 1 second when the start time is 00:00:00 and the end time is 00:01:01", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T00:01:01"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-01T00:01:01")
+      )
+    ).toEqual({
       days: 0,
       hours: 0,
       minutes: 1,
@@ -38,7 +58,12 @@ describe("get-time-since", () => {
   });
 
   it("returns 59 minutes & 59 seconds when the start time is 00:00:00 and the end time is 00:59:59", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T00:59:59"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-01T00:59:59")
+      )
+    ).toEqual({
       days: 0,
       hours: 0,
       minutes: 59,
@@ -47,7 +72,12 @@ describe("get-time-since", () => {
   });
 
   it("returns 1 hour, 1 minute & 1 second when the start time is 00:00:00 and the end time is 01:01:01", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T01:01:01"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-01T01:01:01")
+      )
+    ).toEqual({
       days: 0,
       hours: 1,
       minutes: 1,
@@ -56,7 +86,12 @@ describe("get-time-since", () => {
   });
 
   it("returns 23 hours, 59 minutes & 59 seconds when the start time is 00:00:00 and the end time is 23:59:59", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T23:59:59"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-01T23:59:59")
+      )
+    ).toEqual({
       days: 0,
       hours: 23,
       minutes: 59,
@@ -65,7 +100,12 @@ describe("get-time-since", () => {
   });
 
   it("returns 1 day, 1 hour, 1 minute & 1 second when the start time is 2021-01-01T00:00:00 and the end time is 2021-01-02T01:01:01", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-02T01:01:01"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-02T01:01:01")
+      )
+    ).toEqual({
       days: 1,
       hours: 1,
       minutes: 1,
@@ -74,7 +114,12 @@ describe("get-time-since", () => {
   });
 
   it("returns 6 days, 23 hours, 59 minutes & 59 seconds when the start time is 2021-01-01T00:00:00 and the end time is 2021-01-07T23:59:59", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2021-01-07T23:59:59"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2021-01-07T23:59:59")
+      )
+    ).toEqual({
       days: 6,
       hours: 23,
       minutes: 59,
@@ -83,11 +128,30 @@ describe("get-time-since", () => {
   });
 
   it("returns 365 days when the start time is 2021-01-01T00:00:00 and the end time is 2022-01-01T00:00:00", () => {
-    expect(getTimeSince(new Date("2021-01-01T00:00:00"), new Date("2022-01-01T00:00:00"))).toEqual({
+    expect(
+      getTimeSince(
+        new Date("2021-01-01T00:00:00"),
+        new Date("2022-01-01T00:00:00")
+      )
+    ).toEqual({
       days: 365,
       hours: 0,
       minutes: 0,
       seconds: 0,
+    });
+  });
+
+  it("returns positive values for all units when the start date is in the future", () => {
+    expect(
+      getTimeSince(
+        new Date("2021-01-07T23:59:59"),
+        new Date("2021-01-01T00:00:00")
+      )
+    ).toEqual({
+      days: 6,
+      hours: 23,
+      minutes: 59,
+      seconds: 59,
     });
   });
 });
